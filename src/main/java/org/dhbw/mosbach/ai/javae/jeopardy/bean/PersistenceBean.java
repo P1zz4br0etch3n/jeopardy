1fc44bda7ad3883b02260bbbe3d0546e85f9bd13
lsc <luk.schmelcher@lehre.mosbach.dhbw.de>
2018-07-15 20:30:37 +0200

    added first set of authentication methods

--- src/main/java/org/dhbw/mosbach/ai/javae/jeopardy/bean/PersistenceBean.java
+++ src/main/java/org/dhbw/mosbach/ai/javae/jeopardy/bean/PersistenceBean.java
@@ -9,11 +9,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @Named
 @ApplicationScoped
 public class PersistenceBean {
+    private SecureRandom rnd = new SecureRandom();
+    private HashMap<String, User> TokenToUser = new HashMap<>();
 
     @PersistenceContext
     private EntityManager em;
@@ -38,6 +43,37 @@ public class PersistenceBean {
     @Transactional
     public void deleteUser(User user) {
         em.remove(user);
+    }
+
+    public User authenticateUserByUsernameAndPassword(String username, String password){
+        List<User> allUsers = getAllUsers();
+        for (User user : allUsers){
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User authenticateUserByAuthToken(String authToken){
+        if (TokenToUser.containsKey(authToken)){
+            return TokenToUser.get(authToken);
+        }
+        return null;
+    }
+
+    public String generateUserAuthToken(User user){
+        byte authTokenBytes[] = new byte[32];
+        rnd.nextBytes(authTokenBytes);
+        SetAuthTokenOfUser(user, Arrays.toString(authTokenBytes));
+        return Arrays.toString(authTokenBytes);
+    }
+
+    private void SetAuthTokenOfUser(User user, String authToken){
+        if (TokenToUser.containsKey(authToken) || TokenToUser.containsValue(user)){
+            return;
+        }
+        TokenToUser.put(authToken, user);
     }
 
 
