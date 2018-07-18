109dd544c701dbd219131aaf6f08774a9457a12a
bthebarrel <bernd.hartmann96@gmx.de>
2018-07-19 00:04:19 +0200

    ready for presentation

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from '../../../node_modules/rxjs/Observable';
+import { RequestOptions, Request, RequestMethod } from '@angular/http';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, public http: HttpClient) { }
   @Input() user: string;
   @Input() pwd: string;
   @Input() pwdWdh: string;
@@ -16,17 +19,32 @@ export class RegisterComponent {
 
   clickRegister() {
     console.log(this.user);
-    if (this.user === undefined || this.pwd === undefined  || this.pwdWdh === undefined ||
-      this.user === '' || this.pwd === ''  || this.pwdWdh === '') {
+    if (this.user === undefined || this.pwd === undefined || this.pwdWdh === undefined ||
+      this.user === '' || this.pwd === '' || this.pwdWdh === '') {
       this.emptyInput = true;
     } else {
       if (this.pwd === this.pwdWdh) {
         this.equalPwd = true;
-        this.router.navigate(['/login']);
+        this.sendRegistration(this.user, this.pwd);
+        this.router.navigate(['/jeopardy/rest/login']);
       } else {
         this.equalPwd = false;
       }
     }
   }
 
+  sendRegistration(user: string, pwd: string) {
+    const userdata = {
+      username: user,
+      password: pwd
+    };
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    this.http.post('/users', userdata, httpOptions).subscribe({ error: e => console.error(e) });
+
+  }
 }
