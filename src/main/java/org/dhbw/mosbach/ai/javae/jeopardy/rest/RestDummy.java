c19c3b907ca76bf71f0edb4b112345f0a74ac3e6
lsc <luk.schmelcher@lehre.mosbach.dhbw.de>
2018-07-18 23:44:55 +0200

    implemented genGame to generate dummy games

--- src/main/java/org/dhbw/mosbach/ai/javae/jeopardy/rest/RestDummy.java
+++ src/main/java/org/dhbw/mosbach/ai/javae/jeopardy/rest/RestDummy.java
@@ -23,6 +23,21 @@ public class RestDummy {
             dummyuser.setEmail("dummy@email.tld");
             pb.saveUser(dummyuser);
 
+            User dummyuserAlt = new User();
+            dummyuserAlt.setId(2);
+            dummyuserAlt.setUsername("DummyUserAlt");
+            dummyuserAlt.setPasswordHash("geheim");
+            dummyuserAlt.setEmail("dummyAlt@email.tld");
+            pb.saveUser(dummyuserAlt);
+
+            Game dummygame1 = genGame(10, dummyuser);
+            Game dummygame2 = genGame(100, dummyuser);
+            Game dummygame3 = genGame(1000, dummyuserAlt);
+
+            pb.saveGame(dummygame1);
+            pb.saveGame(dummygame2);
+            pb.saveGame(dummygame3);
+
             Question q1 = new Question();
             q1.setId(10);
             q1.setText("Question1");
@@ -206,5 +221,46 @@ public class RestDummy {
             pb.saveGame(dummygame);
         }
         catch (ScoreNotValidException s) {}
+    }
+
+    private Game genGame(int start, User creator){
+        Game g = new Game();
+        g.setId(++start);
+        g.setName("Game"+start);
+        g.setCreator(creator);
+
+        ArrayList<Category> categories = new ArrayList<>();
+        for(int a = 0; a < 5; a++){
+            Category c = new Category();
+            c.setId(++start);
+            c.setName("Category"+start);
+
+            ArrayList<Question> questions = new ArrayList<>();
+            for (int b = 0; b < 5; b++){
+                Question q = new Question();
+                q.setId(++start);
+                q.setText("Question"+start);
+                q.setAnswer("42");
+
+                try {
+                switch(b) {
+                    case 0: q.setScore(100);  break;
+                    case 1: q.setScore(300);  break;
+                    case 2: q.setScore(500);  break;
+                    case 3: q.setScore(800);  break;
+                    case 4: q.setScore(1000); break;
+                }
+                }catch (Exception e){}
+
+                questions.add(q);
+            }
+
+            c.setQuestions(questions);
+
+            categories.add(c);
+        }
+        g.setCategories(categories);
+
+        return g;
     }
 }
